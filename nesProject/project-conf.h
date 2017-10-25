4d459ddb74ce0ec6396693eab8d995aabc288168
enricomeloni <enrico-meloni@hotmail.it>
2017-10-25 19:10:15 +0200

    Add Project Conf H

---
+++ nesProject/project-conf.h
@@ -0,0 +1,10 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+#undef NETSTACK_CONF_MAC
+#define NETSTACK_CONF_MAC nullmac_driver
+
+#undef NETSTACK_CONF_RDC
+#define NETSTACK_CONF_RDC nullrdc_driver
+
+#endif
