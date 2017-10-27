d2954b3d744d1333565a33eba046dd71cbc7d41d
enricomeloni <enrico-meloni@hotmail.it>
2017-10-27 15:39:55 +0200

    Removed build artifacts from repo

--- nesProject/cmake-build-debug/CMakeFiles/contiki_build.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/contiki_build"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/contiki_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
