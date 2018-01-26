56b8fddc21812fd25a9aaa2954a5cf2d63820fbf
Enrico Meloni <enrico.meloni@outlook.it>
2018-01-26 17:21:44 +0100

    Add smart mailbox node

--- nesProject/central_unit.c
+++ nesProject/central_unit.c
@@ -6,69 +6,77 @@
 #include "dev/button-sensor.h"
 
 #include "commons/constants.h"
+#include "commons/command_process.h"
 #include "central_unit/cuRimeStack.h"
+
 
 PROCESS(central_unit_main, "Central Unit Main Process");
 
-AUTOSTART_PROCESSES(&central_unit_main);
+AUTOSTART_PROCESSES(&central_unit_main, &command_process);
 
 
 void command_switch(unsigned char command)
 {
+    static int isAlarmOn = 0;
+
+    if(command == ALARM_TOGGLE_COMMAND)
+    {
+        printf("Alarm Toggled\n");
+        isAlarmOn = !isAlarmOn;
+        
+        sendDoorNode(&command, 1);
+        sendGateNode(&command, 1);
+
+        return;
+    }
+
+    //if alarm is on, ignore commands
+    if(isAlarmOn)
+    {
+       printf("Alarm is on, command ignored!\n");
+       return; 
+    }
+
     switch(command)
     {
-        case ALARM_TOGGLE_COMMAND:
-		{
-			printf("Alarm Toggled\n");
-			
-			sendDoorNode(&command, 1);
-			sendGateNode(&command, 1);
-
-			break;
-		}
-		
         case GATELOCK_TOGGLE_COMMAND:
-		{
-			printf("Gate Lock Toggled\n");
+        {
+            printf("Gate Lock Toggled\n");
 
-			sendGateNode(&command, 1);
-			
-			break;
-		}
+            sendGateNode(&command, 1);
+            
+            break;
+        }
         
         case DOORS_OPEN_COMMAND:
-		{
-			printf("Doors opened\n");
-			
-			sendDoorNode(&command, 1);
-			sendGateNode(&command, 1);
-			
-			break;
-		}
-		
+        {
+            printf("Doors opened\n");
+            
+            sendDoorNode(&command, 1);
+            sendGateNode(&command, 1);
+            
+            break;
+        }
+        
         case AVERAGE_TEMPERATURE_COMMAND:
-		{
-			printf("Average temp\n");
-	
-			sendDoorNode(&command, 1);
-			
-			//TODO: obtain response from the node
-			
-			break;
-		}
-		
+        {
+            printf("Average temp\n");
+    
+            sendDoorNode(&command, 1);
+            
+            break;
+        }
+        
         case LIGHT_VALUE_COMMAND:
-		{
-			printf("Light Value\n");
-			
-			sendGateNode(&command, 1);
-			
-			//TODO: obtain response from the node
-			
-			break;
-		}
-		
-		default:
+        {
+            printf("Light Value\n");
+            
+            sendGateNode(&command, 1);
+            
+            break;
+        }
+        
+        default:
             printf("There is no command with id %d\n", command);
             break;
     }
@@ -99,45 +107,26 @@ void processGateMessage(unsigned char* message, int payloadSize)
     }
 }
 
+void processMboxMessage(unsigned char* message, int payloadSize)
+{
+    unsigned char cmd = message[0];
+
+    if(cmd == MBOX_EMPTY)
+    {
+        printf("Mailbox is empty\n");
+        leds_off(LEDS_GREEN);
+    }
+    else if(cmd == MBOX_FULL)
+    {
+        printf("Mailbox is full\n");
+        leds_on(LEDS_GREEN);
+    }
+}
+
 PROCESS_THREAD(central_unit_main, ev, data)
 {
-	static struct etimer commandTimeout;
-	static unsigned char buttonCount = 0;
-	
-	PROCESS_BEGIN();
-                SENSORS_ACTIVATE(button_sensor);
-
-				initCURimeStack();
-				
-                while(1)
-				{
-                    PROCESS_WAIT_EVENT();
-
-                    if(ev == sensors_event && data == &button_sensor){
-                        ++buttonCount;
-                        printf("Button pressed %d times\n", buttonCount);
-
-                        if(buttonCount == 1)//first press, set the timer
-                        {
-                            etimer_set( &commandTimeout, COMMAND_TIMEOUT * CLOCK_SECOND );
-                        }
-                        else
-                        {
-                            etimer_restart(&commandTimeout);
-                        }
-                    }
-					else if(ev == PROCESS_EVENT_TIMER)
-                    {
-                        if(buttonCount != 0 && etimer_expired(&commandTimeout))
-						{
-                            printf("Timer expired: count is %d\n", buttonCount);
-                            etimer_stop(&commandTimeout);
-                            command_switch(buttonCount);
-                            buttonCount = 0;
-                        }
-                    }
-                }
-		
-
+    PROCESS_BEGIN();
+	initCURimeStack();
+    leds_off(LEDS_ALL);
     PROCESS_END();
 }
